35ad9b60406a851d8c23de9263439029b534b275
Luoshipeng <913230125@qq.com>
2021-08-09 21:11:25 +0800

    Initial Commit

---
+++ GankGirl/GankGirlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GankGirlApp.swift
+//  GankGirl
+//
+//  Created by 罗诗朋 on 2021/8/9.
+//
+
+import SwiftUI
+
+@main
+struct GankGirlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
